b99aa7ade520ce4e9cc07ebccf3e2dfa715598bc
Coltor Leichtman <coltorleichtman@gmail.com>
2020-07-11 17:11:06 -0700

    vscode and cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <utility>
+#include <algorithm>
 
-int main(int argc, char const *argv[])
+bool modus_ponens(std::pair<std::string, std::string> conditional, std::string antecedent, std::string test);
+
+int main()
 {
-    std::cout << "Hello world" << std::endl;
+
+    std::pair<std::string, std::string> pair("a", "b");
+    std::cout << "test" << std::endl;
+    std::cout << "test" << std::endl;
+
+
+    modus_ponens(pair, "a", "test");
+
     return 0;
+}
+
+bool modus_ponens(std::pair<std::string, std::string> conditional, std::string antecedent, std::string test)
+{
+    test = test.append("test");
+
+    auto ret = false;
+
+    if (conditional.first.compare(antecedent) == 0)
+    {
+        ret = true;
+    }
+
+
+    return ret;
 }
